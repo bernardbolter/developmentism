5d86c0d05730f8492df128f21f445ee92dfecb0a
Bernard Bolter <BeMajor@Bernards-MacBook-Pro.local>
2016-06-21 18:21:25 -0700

    initial commit

---
+++ assets/js/components/entry.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react';
+
+import Logo from './logo';
+
+export default class Entry extends Component {
+  render() {
+    return (
+      <Logo />
+    );
+  }
+}
